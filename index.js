25a112f9c2a32d689691fb5291a74dbb6e3e27fe
SolomonDenning <sdenning@ucmerced.edu>
2021-11-01 16:05:08 -0700

    Added Environment Variables

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const axios = require('axios');
 const helmet = require("helmet");
+require('dotenv').config()
 
 const app = express();
 
 app.use(helmet());
+
+// AUTH INFO
+auth_user_id = process.env.SWEEP_API_ID /// user key from api_keys
+auth_token = process.env.SWEEP_API_TOKEN // token from api_keys
