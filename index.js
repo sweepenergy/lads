edb3cf8253a443d5b75e23b39b096f5564251370
SolomonDenning <sdenning@ucmerced.edu>
2021-11-03 15:39:21 -0700

    GET Home Directory
    Implemented the GET Home Directory in SweepAPI

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ app.use(helmet());
 // AUTH INFO
 auth_user_id = process.env.SWEEP_API_ID /// user key from api_keys
 auth_token = process.env.SWEEP_API_TOKEN // token from api_keys
+
+var config = {
+  method: 'get',
+  url: 'https://api.sweepapi.com/directory/home',
+  headers: { Authorization: `Bearer ${auth_token}` }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
