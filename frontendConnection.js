7635b8911faaac84eab9a7087510ad3b6a4022fb
SolomonDenning <sdenning@ucmerced.edu>
2021-11-30 09:09:04 -0800

    Created frontendConnection.js
    Has GET and POST API calls needed for communication between frontend and backend

---
+++ frontendConnection.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 4000;
+
+// Add the bodyParser middelware to the express application
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Success! Your application is running on port ${port}.`);
+});
+
+app.get('/dockercontainers', (req, res) => {
+    fs.readFile('containersJSON.json', 'utf8', function(err, data){
+        // console.log(data)
+        return res.send(data);
+    });
+    // return res.send('Received a GET HTTP method on Main page');
+});
+  
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+  
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
