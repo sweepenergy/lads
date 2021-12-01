90c37a48ad612f7676b4f068039f26a27ed56a38
SolomonDenning <sdenning@ucmerced.edu>
2021-12-01 12:56:23 -0800

    Added POST API method for frontendConnection
    Allows frontend to send JSON object to backend

--- frontendConnection.js
+++ frontendConnection.js
@@ -5,8 +5,9 @@ const fs = require('fs');
 const app = express();
 const port = 4000;
 
-// Add the bodyParser middelware to the express application
+//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
@@ -15,12 +16,15 @@ app.listen(port, () => {
 app.get('/dockercontainers', (req, res) => {
     fs.readFile('containersJSON.json', 'utf8', function(err, data){
         // console.log(data)
-        return res.send(data);
+        return res.send(JSON.parse(data));
     });
     // return res.send('Received a GET HTTP method on Main page');
 });
   
-app.post('/', (req, res) => {
+app.post('/locationupdates', (req, res) => {
+    // Example Request: 
+    // curl -X POST -H "Content-Type: application/json" -d '{"container": "alpha", "id": "238C88d3nHD6", "logging-ok" : "false"}' localhost:4000/locationupdates
+    console.log(req.body);
     return res.send('Received a POST HTTP method');
 });
   
