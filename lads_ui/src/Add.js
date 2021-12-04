4663374e44b939b5e40e789455b8a86c2807902c
Apsara <38597307+fite40927@users.noreply.github.com>
2021-12-03 17:21:20 -0800

    separate fetch func

--- lads_ui/src/Add.js
+++ lads_ui/src/Add.js
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import fuckme from './AppFetch.js';
 
 // async function getJSON(url) {
 //     const response = await axios.get(url);
@@ -32,29 +33,29 @@ import axios from 'axios';
 
 
 
-function fuckme(url){
-  let request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.responseType = 'json';
-  request.onload = function() {
-    console.log(request.response);
-    var json = (request.response);
+// function fuckme(url, divID){
+//   let request = new XMLHttpRequest();
+//   request.open('GET', url);
+//   request.responseType = 'json';
+//   request.onload = function() {
+//     console.log(request.response);
+//     var json = (request.response);
 
-    var element = document.querySelector("#fucku");
-    element.innerHTML = "";
-    console.log(json);
-    for(var key = 0; key < json.length; key++){
-      console.log(json[key]);
-      element.innerHTML += '<label>'
-      let p = '<div class="row"><div class="col"><input type="checkbox" name=' + json[key].ID + ' onChange={handleChange} id="container"/></div><div class="col">'+json[key].ID+'</div><div class="col">'+json[key].Names+'</div><div class="col">'+json[key].Status+'</div></div>';
-      console.log(p);
-      element.innerHTML += (p);
-      element.innerHTML += '<\label>'
-    }
-  };
-  console.log(request);
-  request.send();
-}
+//     var element = document.querySelector("#fucku");
+//     element.innerHTML = "";
+//     console.log(json);
+//     for(var key = 0; key < json.length; key++){
+//       console.log(json[key]);
+//       element.innerHTML += '<label>'
+//       let p = '<div class="row"><div class="col"><input type="checkbox" name=' + json[key].ID + ' onChange={handleChange} id="container"/></div><div class="col">'+json[key].ID+'</div><div class="col">'+json[key].Names+'</div><div class="col">'+json[key].Status+'</div></div>';
+//       console.log(p);
+//       element.innerHTML += (p);
+//       element.innerHTML += '<\label>'
+//     }
+//   };
+//   console.log(request);
+//   request.send();
+// }
 
 function Add() {
   const formReducer = (state, event) => {
@@ -103,11 +104,11 @@ function Add() {
         					<p><label>File Path: &nbsp;<input name="filepath" onChange={handleChange} value={formData.filepath || ''}/></label></p>
         					: 
         					formData.source == 'docker' ? 
-                      <p><Container id="fucku">{fuckme('http://localhost:4000/dockercontainers')}</Container></p>
+                      <p><Container id="fucku">Containers</Container></p>
                       : 
                       <p></p>
         			}
-        	{console.log(document.querySelector("label"))}
+        	{fuckme('http://localhost:4000/dockercontainers','fucku')}
     			</fieldset>
     			<Button type="submit" value="Submit">Submit</Button>
   			</form>
--- lads_ui/src/Agent.js
+++ lads_ui/src/Agent.js
@@ -4,25 +4,32 @@ import {getJSON} from './testLogs.js';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import fuckme from './AppFetch.js';
+
 
 class Agent extends React.Component {
-	getLogs(){
-		var json = getJSON();
-		//console.log(json);
-		var logs = [];
-		//console.log(json[1].id);
-		for (let key in json){
-			logs.push(<Row><Col>{json[key].ID}</Col><Col>{json[key].Names}</Col><Col>{json[key].Status}</Col></Row>);
-		}
-		return (logs);
+	constructor(props){
+		super(props);
+
+		fuckme('http://localhost:4000/dockercontainers','wah');
 	}
+	// getLogs(){
+	// 	var json = getJSON();
+	// 	//console.log(json);
+	// 	var logs = [];
+	// 	//console.log(json[1].id);
+	// 	for (let key in json){
+	// 		logs.push(<Row><Col>{json[key].ID}</Col><Col>{json[key].Names}</Col><Col>{json[key].Status}</Col></Row>);
+	// 	}
+	// 	return (logs);
+	// }
 		
 	render(){
 		return(
 			<div className="Agent">
 				<Container className="Agent-header">Agent {this.props.id}</Container>
-				<Container className="Agent-body">
-					{this.getLogs()}
+				<Container className="Agent-body" id="wah">
+					wah
 				</Container>
 			</div>
 		);
